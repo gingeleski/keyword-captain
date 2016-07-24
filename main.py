1d29c04cd3a5b43ba6a7e8965f9d05dfed0e2265
Randy Gingeleski <rjg26247@gmail.com>
2016-07-24 14:44:12 -0400

    write basic i/o for word list

---
+++ main.py
@@ -0,0 +1,26 @@
+from copy import copy
+
+def loadWordList(wordfile):
+    wordlist = None
+    with open(wordfile) as f:
+        wordlist = f.readlines()
+    return wordlist
+
+def cleanWordList(wordlist):
+    working_list = copy(wordlist)
+    for word in working_list:
+        if (len(word) < 3) or (len(word) > 10):
+            working_list.remove(word)
+            print('removing ', word)
+        elif "'" in word:
+            working_list.remove(word)
+            print('removing ', word)
+    return working_list
+
+def saveWordList(filename, wordlist):
+    file = open(filename, "w")
+    for word in wordlist:
+        file.write(word)
+    file.close()
+
+wordlist = loadWordList("words.txt")
