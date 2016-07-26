e24285f6804dd397baf0355475d4d5ac7f8ef822
Randy Gingeleski <rjg26247@gmail.com>
2016-07-25 21:50:14 -0400

    Cells now have fields for letters and score

--- Cell.py
+++ Cell.py
@@ -1,7 +1,15 @@
 class Cell(object):
-    value = None
-    is_used = None
-    
+    letters = None
+    value = 0
+    is_used = False
+
     def __init__(self):
-        self.value = 'X'
+        self.letters = 'X'
+        self.value = 0
         self.is_used = False
+
+    def get_letters(self):
+        return self.letters
+
+    def get_value(self):
+        return self.value
