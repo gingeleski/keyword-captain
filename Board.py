dc811163586f989f659ca4a5d4ec975ecc31c0c7
Randy Gingeleski <rjg26247@gmail.com>
2016-07-27 21:41:49 -0400

    Board coord init and neighbor lookup

--- Board.py
+++ Board.py
@@ -3,10 +3,22 @@ from Cell import Cell
 class Board(object):
     board = []
 
-    def __init__(self):
+    def __init__(self, size):
         self.board = []
-        for i in range(5):
+        for i in range(size):
             cl = []
-            for j in range(5):
-                cl.append(Cell())
+            for j in range(size):
+                this_cell = Cell()
+                this_cell.set_x(j)
+                this_cell.set_y(i)
+                cl.append(this_cell)
             self.board.append(cl)
+    
+    def get_neighbors(self, cell):
+        neighbors = []
+        for i in range(rowNumber-2, rowNumber+1):
+            for j in range(columnNumber-2, columnNumber+1):
+                if  i >= 0 and i < len(self.board):
+                    if j >= 0 and j < len(self.board[0]):
+                        neighbors.append(self.board[i][j])
+        return neighbors
