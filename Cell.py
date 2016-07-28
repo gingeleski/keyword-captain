042138f88d8d8ccb265215b11fc8f812e62691ee
Randy Gingeleski <rjg26247@gmail.com>
2016-07-27 21:40:57 -0400

    updates to Cell, including coordinates

--- Cell.py
+++ Cell.py
@@ -1,15 +1,30 @@
 class Cell(object):
-    letters = None
-    value = 0
+    letters = ''
+    value = -1
     is_used = False
 
+    # coordinates on board
+    x = -1
+    y = -1
+
     def __init__(self):
-        self.letters = 'X'
-        self.value = 0
+        self.letters = ''
+        self.value = -1
         self.is_used = False
+        self.x = -1
+        self.y = -1
 
     def get_letters(self):
         return self.letters
 
     def get_value(self):
         return self.value
+
+    def get_coordinates(self):
+        return self.x, self.y
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
