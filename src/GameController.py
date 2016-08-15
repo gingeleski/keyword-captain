ce3645b185fee4fd79aec87aaac3ae76e8f3ffa2
Randy Gingeleski <rjg26247@gmail.com>
2016-08-15 06:05:18 -0400

    stub out new HUD and GameController classes

---
+++ src/GameController.py
@@ -0,0 +1,7 @@
+class GameController(object):
+    def __init__(self):
+        """
+        Static class, constructor doesn't really
+        do anything.
+        """
+        pass
---
+++ src/HUD.py
@@ -0,0 +1,10 @@
+class HUD(object):
+    """
+    Heads Up Display graphical user interface
+    """
+    def __init__(self):
+        """
+        Static class, constructor doesn't really
+        do anything.
+        """
+        pass
