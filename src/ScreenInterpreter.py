14e4a143ee3639817a04e21ecd958bbd30746cb4
Randy Gingeleski <rjg26247@gmail.com>
2016-08-19 19:35:31 -0400

    calibration and further work on ScrabbleBoggleInterpreter

--- src/ScreenInterpreter.py
+++ src/ScreenInterpreter.py
@@ -23,39 +23,55 @@ class ScreenInterpreter(object):
 class ScrabbleBoggleInterpreter(ScreenInterpreter):
     game_area_top_left = os.path.abspath("../resources/scrabble-boggle_img/game-area_top-left.png")
     game_area_bottom_right = os.path.abspath("../resources/scrabble-boggle_img/game-area_bottom-right.png")
+
+    game_area_top_left_x = -1
+    game_area_top_left_y = -1
+    game_area_bottom_right_x = -1
+    game_area_bottom_right_y = -1
     
     def __init__(self):
         ScreenInterpreter.__init__(self)
 
-    def locateBoard(self, img):
+    def locateGameArea(self, img=None):
         """
-        Locates the game board on a given image
+        Locates the game area on a given image, or the current screen
+        if no image path is passed
 
         Args:
-            img (str) - image file path
+            img (str) - optional, image file path
 
         Returns:
             (Tuple) - top left x, top left y, bottom right x, bottom right y
         """
-        top_left = pag.locate(self.game_area_top_left, img)
-        bottom_right = pag.locate(self.game_area_bottom_right, img)
+        if img is None:
+            top_left = pag.locateOnScreen(self.game_area_top_left)
+            bottom_right = pag.locateOnScreen(self.game_area_bottom_right)
+        else:
+            top_left = pag.locate(self.game_area_top_left, img)
+            bottom_right = pag.locate(self.game_area_bottom_right, img)
 
-        if top_left is None or bottom_right is None:
-            return None
+        if top_left is not None and bottom_right is not None:
+            self.game_area_top_left_x = top_left[0]
+            self.game_area_top_left_y = top_left[1]
+            self.game_area_bottom_right_x = bottom_right[0] + bottom_right[2]
+            self.game_area_bottom_right_y = bottom_right[1] + bottom_right[3]
 
-        return top_left[0], top_left[1], bottom_right[0], bottom_right[1]
-
-    def locateBoardOnScreen(self):
+    def isolateLetterBoard(self, img):
         """
-        Locates the game board on current screen
-
-        Returns:
-            (Tuple) - top left x, top left y, bottom right x, bottom right y
+        Given an image of the screen, returns a cropped image of just
+        the letter board
         """
-        top_left = pag.locateOnScreen(self.game_area_top_left)
-        bottom_right = pag.locateOnScreen(self.game_area_bottom_right)
+        top_x = self.game_area_top_left_x + 263
+        top_y = self.game_area_top_left_y + 81
+        bottom_x = self.game_area_bottom_right_x - 90
+        bottom_y = self.game_area_bottom_right_y - 50
 
-        if top_left is None or bottom_right is None:
-            return None
+        return img.crop((top_x, top_y, bottom_x, bottom_y))
 
-        return top_left[0], top_left[1], bottom_right[0], bottom_right[1]
+sbi = ScrabbleBoggleInterpreter()
+the_path = os.path.abspath("../resources/scrabble-boggle_img/sample-fullscreen.png")
+sbi.locateGameArea(the_path)
+
+im = Image.open(the_path)
+im_cropped = sbi.isolateLetterBoard(im)
+im_cropped.show()
